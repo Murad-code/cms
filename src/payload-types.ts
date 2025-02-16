afd74340c1a8459e566d84cb29a64254b17ef09b
Murad Kamali <murad.coding@gmail.com>
2025-02-16 17:46:26 +0000

    adds s3 storage plugin to config

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -228,6 +228,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1113,6 +1114,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
